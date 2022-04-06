7c8f25e0698727f1e9a3f29d12156ce4db642a7c
Enrique Mendez <eqm@mit.edu>
2022-04-06 14:14:15 -0400

    Reran ./get_capabilities.py generate_subclasses.py

--- NI_DAQmx/models/NI_PCI_6143.py
+++ NI_DAQmx/models/NI_PCI_6143.py
@@ -24,30 +24,42 @@ from labscript_devices.NI_DAQmx.labscript_devices import NI_DAQmx
 
 #:
 CAPABILITIES = {
-    'AI_range': None,
-    'AI_range_Diff': None,
-    'AI_start_delay': None,
-    'AO_range': [-10.0, 10.0],
-    'max_AI_multi_chan_rate': None,
-    'max_AI_single_chan_rate': None,
-    'max_AO_sample_rate': 800000.0,
+    'AI_range': [-5.0, 5.0],
+    'AI_range_Diff': [-5.0, 5.0],
+    'AI_start_delay': 1e-07,
+    'AI_term': 'Diff',
+    'AI_term_cfg': {
+        'ai0': ['Diff'],
+        'ai1': ['Diff'],
+        'ai2': ['Diff'],
+        'ai3': ['Diff'],
+        'ai4': ['Diff'],
+        'ai5': ['Diff'],
+        'ai6': ['Diff'],
+        'ai7': ['Diff'],
+    },
+    'AO_range': None,
+    'max_AI_multi_chan_rate': 250000.0,
+    'max_AI_single_chan_rate': 250000.0,
+    'max_AO_sample_rate': None,
     'max_DO_sample_rate': None,
     'min_semiperiod_measurement': 2e-05,
-    'num_AI': 0,
-    'num_AO': 32,
+    'num_AI': 8,
+    'num_AO': 0,
     'num_CI': 2,
     'ports': {'port0': {'num_lines': 8, 'supports_buffered': False}},
-    'supports_buffered_AO': True,
+    'supports_buffered_AO': False,
     'supports_buffered_DO': False,
     'supports_semiperiod_measurement': True,
+    'supports_simultaneous_AI_sampling': True,
 }
 
 
-class NI_PCI_6723(NI_DAQmx):
-    description = 'NI-PCI-6723'
+class NI_PCI_6143(NI_DAQmx):
+    description = 'NI-PCI-6143'
 
     def __init__(self, *args, **kwargs):
-        """Class for NI-PCI-6723"""
+        """Class for NI-PCI-6143"""
         # Any provided kwargs take precedent over capabilities
         combined_kwargs = CAPABILITIES.copy()
         combined_kwargs.update(kwargs)
